59fbf5628627e38b4cf71d31577f3128ca2103f2
rxtur <rtur.net@gmail.com>
2019-07-18 23:32:55 -0500

    Title renaming

--- simple/README.md
+++ simple/README.md
@@ -1,4 +1,4 @@
-# Foo
+# simple - Angular CLI theme for Blogifier
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.0.
 
--- simple/src/app/app.component.spec.ts
+++ simple/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'foo'`, () => {
+  it(`should have as title 'simple'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('foo');
+    expect(app.title).toEqual('simple');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to foo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to simple!');
   });
 });
