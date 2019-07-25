1c136dc6c40e2b31f3d60389998478b3ed38d19c
rxtur <rtur.net@gmail.com>
2019-07-24 20:57:12 -0500

    Post list loaded from api, component tweaks

--- standard/src/app/home/home.component.ts
+++ standard/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from '../core/blog.service';
-import { IBlogSettings } from '../core/blog.models';
+import { IBlogSettings, IPostList } from '../core/blog.models';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +8,7 @@ import { IBlogSettings } from '../core/blog.models';
 })
 export class HomeComponent implements OnInit {
   public blogSettings: IBlogSettings;
+  public postList: IPostList;
   errorMessage = '';
 
   constructor(private blogService: BlogService) { }
@@ -17,5 +18,16 @@ export class HomeComponent implements OnInit {
       result => { this.blogSettings = result; },
       error => this.errorMessage = <any>error
     );
+    this.blogService.getPosts().subscribe(
+      result => { this.postList = result; },
+      error => this.errorMessage = <any>error
+    );
+  }
+
+  toDate(date): string {
+    var monthNames = ["January", "February", "March", "April", "May", "June", 
+      "July", "August", "September", "October", "November", "December"];
+    var d = new Date(date); 
+    return monthNames[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear();
   }
 }
