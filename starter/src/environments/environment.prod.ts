94fe577ab1700df2782470993d23b7cc86b37234
rxtur <rtur.net@gmail.com>
2019-08-10 22:41:26 -0500

    Starter theme removed

--- starter/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`
-// set to empty string to pull posts via API from the same PROD server 
-export const environment = {
-  production: true,
-  apiEndpoint: ''
-};
--- starter/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`
-// set to pull posts via API from blogifier.net in dev mode 
-
-export const environment = {
-  production: false,
-  apiEndpoint: 'http://blogifier.net'
-};
