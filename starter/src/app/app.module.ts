3a9ce6407258dd6ec9a2d494f13ed79896f262a1
rxtur <rtur.net@gmail.com>
2019-08-13 22:36:50 -0500

    Added search and newsletter components

--- starter/src/app/app.module.ts
+++ starter/src/app/app.module.ts
@@ -2,23 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule }   from '@angular/forms';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PostsComponent } from './posts/posts.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { SearchComponent } from './core/search/search.component';
+import { NewsletterComponent } from './core/newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PostsComponent,
-    NotfoundComponent
+    NotfoundComponent,
+    SearchComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
