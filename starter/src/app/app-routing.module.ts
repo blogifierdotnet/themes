5b7940a19594242dd09873a2d7be6f7b990b4558
rxtur <rtur.net@gmail.com>
2019-08-18 20:15:58 -0500

    Theme updates

--- starter/src/app/app-routing.module.ts
+++ starter/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PostsComponent } from './posts/posts.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { ContactComponent } from './core/contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, 
-  { path: 'posts/:slug', component: PostsComponent },
+	{ path: 'posts/:slug', component: PostsComponent },
+	{ path: 'contact', component: ContactComponent },
   { path: '**', component: NotfoundComponent }
 ];
 
