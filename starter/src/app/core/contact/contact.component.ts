bfd1177176449158894158841fa58d25189506b3
rxtur <rtur.net@gmail.com>
2019-08-28 19:02:52 -0500

    Contact component updated in the starter theme

--- starter/src/app/core/contact/contact.component.ts
+++ starter/src/app/core/contact/contact.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BlogService, IContact } from '../blog.service';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+	selector: 'app-contact',
+	templateUrl: './contact.component.html',
+	styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+	public contact: IContact;
 
-  ngOnInit() {
-  }
+	constructor(private blogService: BlogService) { }
+
+	ngOnInit() {
+		this.contact = { name: "", email: "", content: "" };
+	}
+
+	onSubmit() {
+		if (this.contact.name && this.contact.email && this.contact.content) {
+			//this.blogService.contact(this.contact).subscribe(
+			//	() => {
+			//		this.contact = { name: "", email: "", content: "" };
+			//		alert('Thank you!');
+			//	}
+			//	);
+			this.blogService.showMessage("done", true);
+		}
+	}
 
 }
