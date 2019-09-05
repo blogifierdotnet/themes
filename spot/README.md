23eb61eb989a6ea8aab8b7992736d952e194d12e
rxtur <rtur.net@gmail.com>
2019-09-04 19:42:06 -0500

    Renames and build

--- spot/README.md
+++ spot/README.md
@@ -1,4 +1,4 @@
-# Starter
+# Spot
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.0.
 
--- spot/karma.conf.js
+++ spot/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/starter'),
+      dir: require('path').join(__dirname, './coverage/spot'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
